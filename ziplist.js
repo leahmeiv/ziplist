fb6ff3727dc1b6a1000683ebc92e6f303e6333ad
Leah-Mei Villanueva <leahmei@hawaii.edu>
2018-09-20 09:30:17 -1000

    Added Comments

--- ziplist.js
+++ ziplist.js
@@ -1,17 +1,21 @@
 
+  // Two lists of equal length
   const listOne = ['a','b','c'];
   const listTwo = [1,2,3];
 
+  // Function accepts two lists of equal length
   function zipList(listA, listB) {
     const newList = [];
     for (let i = 0; i <listA.length; i++ ){
       newList.push(listA[i], listB[i]);
     }
+    // returns the result of alternatingly taking elements
     return newList;
   }
 
   console.log(zipList(listOne,listTwo));
 
+  // Accepts two lists using underscore
   function zipListTheSimpleWay(listA, listB){
     return _.flatten(_.zip(listA, listB));
   }
